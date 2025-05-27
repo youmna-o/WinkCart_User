025ada9806f8af002126f8848cefd89530bd7bee
Rofaida <rofaidasobhy123@gmail.com>
2025-05-27 18:41:19 +0300

    Settings Screen UI

---
+++ app/src/main/java/com/example/winkcart_user/Utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.winkcart_user.Utils
+
+import androidx.compose.ui.unit.dp
+
+object Constants{
+
+    val SCREEN_PADDING = 16.dp
+
+}
--- app/src/main/java/com/example/winkcart_user/ui/theme/Color.kt
+++ app/src/main/java/com/example/winkcart_user/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val BackgroundColor = Color(0xFFF9F9F9)
+val HeaderTextColor = Color(0xFF222222)
+val CardBackgroundColor = Color(0xFFFFFFFF)
