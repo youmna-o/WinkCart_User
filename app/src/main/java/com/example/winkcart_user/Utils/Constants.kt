52052cc0467a139c034b7d5ef58ea2b85d684bbe
Rofaida <rofaidasobhy123@gmail.com>
2025-05-27 21:27:56 +0300

    Contact us screen UI and usinge string file in About us screen

--- app/src/main/java/com/example/winkcart_user/Utils/Constants.kt
+++ app/src/main/java/com/example/winkcart_user/Utils/Constants.kt
@@ -5,5 +5,7 @@ import androidx.compose.ui.unit.dp
 object Constants{
 
     val SCREEN_PADDING = 16.dp
+    val CONTACT_US_EMAIL = "rofaidasobhy123@gmail.com"
+    val CONTACT_US_PHONE = "tel:+201140708568"
 
 }
--- app/src/main/java/com/example/winkcart_user/settings/ContactUsView.kt
+++ app/src/main/java/com/example/winkcart_user/settings/ContactUsView.kt
@@ -1,34 +1,34 @@
 package com.example.winkcart_user.settings
 
-import androidx.compose.foundation.Image
+import android.content.Intent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.winkcart_user.R
+import com.example.winkcart_user.Utils.Constants
 import com.example.winkcart_user.Utils.Constants.SCREEN_PADDING
 import com.example.winkcart_user.ui.theme.BackgroundColor
 import com.example.winkcart_user.ui.theme.HeaderTextColor
+import androidx.core.net.toUri
 
 @Preview
 @Composable
-private fun ContactUsView() {
+private fun ContactUsScreen() {
+    val localContext = LocalContext.current
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -37,59 +37,30 @@ private fun ContactUsView() {
     ) {
         Column(
             modifier = Modifier
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally
-
-
+                .fillMaxSize()
         ) {
 
+            Text(text= stringResource(R.string.contact_us),color = HeaderTextColor , fontSize = 32.sp, fontWeight = FontWeight.Bold )
+            Spacer(modifier = Modifier.height(30.dp))
+            Text(stringResource(R.string.contact_us_st))
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(stringResource(R.string.contact_us_email), modifier = Modifier.clickable {
+                val intent = Intent(Intent.ACTION_SENDTO).apply {
+                    data = Constants.CONTACT_US_EMAIL.toUri()
+                }
+                localContext.startActivity(intent)
+            })
+
             Spacer(modifier = Modifier.height(8.dp))
-            Image(
-                painter = painterResource(R.drawable.ic_logo),
-                contentDescription = "app Icon",
-                modifier = Modifier
-                    .size(150.dp)
-                    .clip(RoundedCornerShape(16.dp))
 
-            )
+            Text(stringResource(R.string.contact_us_phone), modifier = Modifier.clickable {
+                val intent = Intent(Intent.ACTION_DIAL).apply {
+                    data = Constants.CONTACT_US_PHONE.toUri()
+                }
+                localContext.startActivity(intent)
+            })
 
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = "WinkyCart is a smart shopping app built to simplify your online shopping experience. " +
-                        "We provide access to a wide range of products across fashion, tech, and lifestyle categories.",
-                fontSize = 16.sp
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = "This app was developed as our graduation project at the Information Technology Institute (ITI).",
-                fontSize = 16.sp
-            )
-
-            Spacer(modifier = Modifier.height(24.dp))
-
-            Text(
-                text = "Development Team:",
-                fontSize = 20.sp,
-                fontWeight = FontWeight.SemiBold,
-                color = HeaderTextColor
-            )
-
-            Spacer(modifier = Modifier.height(12.dp))
-
-            val teamMembers = listOf(
-                "Arwa Khaled Mohamed",
-                "Rofaida Sobhy Adwi",
-                "Mustafa Mahmoud Ahmed",
-                "Yomna Ashraf Elsayed"
-            )
-
-            teamMembers.forEach {
-                Text(
-                    text = it,
-                    fontSize = 16.sp,
-                    color = HeaderTextColor
-                )
-            }
 
 
         }
