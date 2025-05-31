cb53755ef66d49d37f334a77b7fd2f5b967ea2db
Arwakh <khaledarwa14@gmail.com>
2025-06-01 00:48:56 +0300

    -edit product info details

--- app/src/main/java/com/example/winkcart_user/ui/home/vendorProducts/views/VendorProducts.kt
+++ app/src/main/java/com/example/winkcart_user/ui/home/vendorProducts/views/VendorProducts.kt
@@ -2,7 +2,12 @@ package com.example.winkcart_user.ui.home.vendorProducts.views
 
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -10,11 +15,13 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -82,16 +89,40 @@ fun VendorProductsOnScuccess(products: List<ProductAbstracted>, vendor: String, 
         }
 
     ) { padding ->
-        LazyColumn(
-            contentPadding = padding,
-            modifier = Modifier.fillMaxSize()
-        ) {
-            items(products.size) { index ->
-                ProductItem(product = products[index], onProductItemClicked = {navController.navigate("ProductInfo/${products[index].id}")})
+        if (products.isNotEmpty()) {
+            LazyColumn(
+                contentPadding = padding,
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                items(products.size) { index ->
+                    ProductItem(product = products[index], onProductItemClicked = { navController.navigate("ProductInfo/${products[index].id}") })
+                }
+            }
+        } else {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text("  All products from this vendor are currently out of stock. ",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = Color.Gray
+                    )
+                    Text(" Stay tuned for updates!",style = MaterialTheme.typography.bodyMedium,
+                        color = Color.Gray
+                    )
+                }
+
+
+
             }
         }
+        }
+
     }
-}
+
 
 @Composable
 fun VendorProductScreenOnLoading() {
